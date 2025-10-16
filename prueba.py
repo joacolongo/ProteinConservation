dc8e3a54a78f2f661f6f935b41368d6eb39a75e0
Joaquin Algorta Bove <joaco.algorta@gmail.com>
2025-10-16 17:31:44 +0200

    updated prueba.py

--- prueba.py
+++ prueba.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     print("Hello World!")
+
+
+def greet(name):
+    """Print a greeting message with the given name"""
+    print("Hello " + name + "!")
+
+
+greet("Alice")
